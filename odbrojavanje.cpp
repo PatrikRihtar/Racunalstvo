054c18f515e824f2b7bc412ba7cd89b10d64e92e
PatrikRihtar <55491452+PatrikRihtar@users.noreply.github.com>
2019-09-18 14:26:58 +0200

    Add files via upload

---
+++ odbrojavanje.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	int s;
+	int ukupno;
+
+
+	
+	cout<<"Unesite sekunde"<<endl;
+	cin>>s;
+
+
+	
+		for(int sek = s; s>=0; s--)
+		{
+			
+			cout<<" Preostalo je "<<" sekundi "<<s<<endl;
+			Sleep(200);
+			
+			if(s ==0)
+			{
+			 {
+		 cout<<" Preostalo sekundi "<<ukupno<<endl;
+				 Sleep(200);
+			 }
+
+
+
+			}
+		}
+
+	
+
+
+
+	 system("pause");
+
+
+}
