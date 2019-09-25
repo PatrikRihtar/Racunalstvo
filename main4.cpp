7db290dcbbabab3e4a4b4ded9f7d2d6d951b7742
PatrikRihtar <55491452+PatrikRihtar@users.noreply.github.com>
2019-09-25 14:13:43 +0200

    Add files via upload

---
+++ main4.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+    
+#include<iostream>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+float manji(float a, float b)
+{
+    if(a>b){
+            return b;
+            }
+            else{
+                 return a;
+                 }
+}
+int main()
+{
+    float a;
+    float b;
+    float rezultat;
+    cout<<"Unesite 2 broja koja želite dijeliti"<<endl;
+    cin>>a;
+    cin>>b;
+    
+    rezultat=manji(a, b);
+    cout<<rezultat;
+    system ("PAUSE");
+}
