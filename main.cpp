a8cfbfbec57d093186f192381c22268212082917
PatrikRihtar <55491452+PatrikRihtar@users.noreply.github.com>
2019-10-23 23:34:33 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int x;
+	int A[6]={0,1,2,3,4,5};
+	for(x=4;x>=0;x--)
+	{
+		cout<<"A["<<x<<"]="<<A[x]<<"  ";
+	}
+
+	
+system("PAUSE"); 
+return 0;
+}
