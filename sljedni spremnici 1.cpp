6b83bb103ee1927e64c9f8216a19d38c41ea88dc
PatrikRihtar <55491452+PatrikRihtar@users.noreply.github.com>
2020-04-07 19:05:20 +0200

    Add files via upload

---
+++ sljedni spremnici 1.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+
+{
+	vector<int> PR;
+	float P;
+	int R=0;
+	cout<<"Ovo je program koji ispisuje brojeve sve dok ne unesete nulu i ispisuje kapacitet."<<endl;
+	cout<<"Unesi neki broj:"<<endl;
+	cin>>P;
+	cout<<endl;
+	while ((P!=0))
+	{
+		if (P!=int(P))
+		{
+			cout<<"Unesi ponovno cijeli broj."<<endl;
+			cin>>P;
+		}
+		else
+		{
+			cout<<"Unesi ponovno dok nebude nula."<<endl;
+			PR.push_back(P);
+			cin>>P;
+		}
+ 	}
+ 	PR.push_back(P);
+ 	cout<<endl;
+	cout<<"Vasi brojevi su:"<<endl;
+	cout<<endl;
+	for (vector<int>::iterator it=PR.begin(); it!=PR.end(); it++)
+	{
+		cout<<*it<<" ";
+		if (PR.size())
+		{
+			R++;
+		}
+	}
+	cout<<"U ovom vektoru ima: "<<R<<" brojeva."<<endl;
+	cout<<"Kapacitet ovih brojeva je: "<<PR.capacity()<<" ."<<endl;
+	return 0;
+}
