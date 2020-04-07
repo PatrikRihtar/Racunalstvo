c9c9ecf5051ea3b82f1772e70ad5f5cd5ce58125
PatrikRihtar <55491452+PatrikRihtar@users.noreply.github.com>
2020-04-07 19:32:57 +0200

    Add files via upload

---
+++ sljedni spremnici 3.cpp
@@ -0,0 +1,68 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+
+{
+	vector<int> PR;
+	float P;
+	int R=0;
+	cout<<"Ovo je program koji ispisuje brojeve sve dok ne unesete nulu i ispisuje kapacitet."<<endl;
+	cout<<"Unesi neki broj:"<<endl;
+	cin>>P;
+	cout<<endl;
+	while ((P!=0))
+	{
+		if (P!=int(P))
+		{
+			cout<<"Unesi ponovno cijeli broj."<<endl;
+			cin>>P;
+		}
+		else
+		{
+			cout<<"Unesi ponovno dok nebude nula."<<endl;
+			PR.push_back(P);
+			cin>>P;
+		}
+ 	}
+ 	PR.push_back(P);
+ 	cout<<endl;
+	cout<<"Vasi brojevi su:"<<endl;
+	cout<<endl;
+	for (vector<int>::iterator it=PR.begin(); it!=PR.end(); it++)
+	{
+		cout<<*it<<" ";
+		if (PR.size())
+		{
+			R++;
+		}
+		
+	}
+	cout<<"U ovom vektoru ima: "<<R<<" brojeva."<<endl;
+	cout<<endl;
+	cout<<"Kapacitet ovih brojeva je: "<<PR.capacity()<<" ."<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"Nakon sortiranja, brojevi izgledaju ovako:"<<endl;
+	sort (PR.begin(), PR.end());
+	for (vector<int>::iterator it=PR.begin(); it!=PR.end(); it++)
+	{
+		cout<<*it<<" ";
+	}
+	cout<<endl;
+	cout<<"Najmanji broj u vektoru je: "<<*min_element(PR.begin(), PR.end())<<" ."<<endl;
+	cout<<"Najveci broj u vektoru je: "<<*max_element(PR.begin(), PR.end())<<" ."<<endl;
+	vector<int>::iterator ip;
+	ip=unique(PR.begin(), PR.end());
+	PR.resize(distance(PR.begin(), ip));
+	cout<<"Brojevi bez ponavljanja su:"<<endl;
+	for (ip=PR.begin(); ip!=PR.end(); ip++)
+	{
+		cout<<*ip<<" ";
+	}
+	return 0;
+}
